df2532d950b2e15fae9847fe8b882af10c897373
Barbara <gimemesb@gmail.com>
2024-09-13 18:13:40 -0300

    Sessão 2 Spring boot core e framework

---
+++ src/main/java/io/github/dougllasfps/Repository/ClientesRepository.java
@@ -0,0 +1,10 @@
+package io.github.dougllasfps.Repository;
+import io.github.dougllasfps.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente){
+        //acessa a base e salva o cliente
+    }
+}
