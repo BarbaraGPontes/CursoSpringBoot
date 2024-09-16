b907c93633c804897abfa990c8740a298ca6eed3
Barbara <gimemesb@gmail.com>
2024-09-16 16:22:50 -0300

    uso de spring jpa em repository e entity

--- src/main/java/io/github/dougllasfps/VendasApplication.java
+++ src/main/java/io/github/dougllasfps/VendasApplication.java
@@ -1,21 +1,48 @@
 package io.github.dougllasfps;
 
+import io.github.dougllasfps.domain.entity.Cliente;
+import io.github.dougllasfps.domain.entity.Pedido;
+import io.github.dougllasfps.domain.repository.Clientes;
+import io.github.dougllasfps.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
-    @Value("${application.name}")
-    private String applicationName;
-    @GetMapping("/hello")
-    public String helloWorld(){
-        return applicationName;
+
+    @Bean
+    public CommandLineRunner init(
+            @Autowired Clientes clientes,
+            @Autowired Pedidos pedidos
+    ) {
+        return args -> {
+            System.out.println("Salvando clientes");
+            Cliente fulano = new Cliente("Fulano");
+            clientes.save(fulano);
+
+            Pedido p = new Pedido();
+            p.setCliente(fulano);
+            p.setDataPedido(LocalDate.now());
+            p.setTotal(BigDecimal.valueOf(100));
+
+            pedidos.save(p);
+
+//            Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
+//            System.out.println(cliente);
+//            System.out.println(cliente.getPedidos());
+
+            pedidos.findByCliente(fulano).forEach(System.out::println);
+
+
+
+        };
     }
 
     public static void main(String[] args) {
